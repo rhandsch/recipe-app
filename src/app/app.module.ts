10038616767381c6f4bea66eb8a308cd3a81b09b
Robert Handschmann <robert.handschmann@gmail.com>
2020-02-09 17:58:06 +0100

    Add dynamic alert component by code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import {AuthComponent} from './auth/auth.component';
 import {LoadingSpinnerComponent} from './shared/loading-spinner/loading-spinner.component';
 import {AuthInterceptorService} from './auth/auth-interceptor.service';
 import {AlertComponent} from './shared/alert/alert.component';
+import {PlaceholderDirective} from './shared/placeholder.directive';
 
 @NgModule({
   declarations: [
@@ -38,6 +39,7 @@ import {AlertComponent} from './shared/alert/alert.component';
     RecipeEditComponent,
     AuthComponent,
     LoadingSpinnerComponent,
+    PlaceholderDirective,
     AlertComponent
   ],
   imports: [
@@ -61,7 +63,10 @@ import {AlertComponent} from './shared/alert/alert.component';
       multi: true
     }
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [
+    AlertComponent
+  ]
 })
 export class AppModule {
 }
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,22 +1,25 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ComponentFactoryResolver, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {NgForm} from '@angular/forms';
 import {AuthResponseData, AuthService} from './auth.service';
-import {Observable} from 'rxjs';
+import {Observable, Subscription} from 'rxjs';
 import {Router} from '@angular/router';
+import {AlertComponent} from '../shared/alert/alert.component';
+import {PlaceholderDirective} from '../shared/placeholder.directive';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css']
 })
-export class AuthComponent implements OnInit {
+export class AuthComponent implements OnInit, OnDestroy {
 
   isLoginMode = true;
   isLoading = false;
   error: string = null;
+  @ViewChild(PlaceholderDirective, {static: false}) placeholder;
+  private closeSub: Subscription;
 
-
-  constructor(private authService: AuthService, private router: Router) {
+  constructor(private authService: AuthService, private router: Router, private componentFactoryResolver: ComponentFactoryResolver) {
   }
 
   ngOnInit() {
@@ -40,6 +43,7 @@ export class AuthComponent implements OnInit {
       },
       errorMessage => {
         this.error = errorMessage;
+        this.showErrorAlert(errorMessage);
         this.isLoading = false;
       });
     authForm.reset();
@@ -52,4 +56,25 @@ export class AuthComponent implements OnInit {
   onAcceptError() {
     this.error = null;
   }
+
+  ngOnDestroy(): void {
+    if (this.closeSub) {
+      this.closeSub.unsubscribe();
+    }
+  }
+
+  private showErrorAlert(errorMessage: string) {
+    const cfr = this.componentFactoryResolver.resolveComponentFactory(AlertComponent);
+    const placeholderRef = this.placeholder.viewContainerRef;
+    placeholderRef.clear();
+
+    const componentRef = placeholderRef.createComponent(cfr);
+    componentRef.instance.message = errorMessage;
+    this.closeSub = componentRef.instance.close.subscribe(() => {
+      this.closeSub.unsubscribe();
+      placeholderRef.clear();
+      this.onAcceptError();
+    });
+  }
+
 }
